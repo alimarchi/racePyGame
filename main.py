b5f47b6e0948139a9d2e00979445ba5b85b70fca
Alice <marchi_alice@yahoo.com>
2022-01-17 19:42:29 +0100

    Main and images

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame, sys 
+
+class Game():
+    runners = []
+    __startLine = 20
+    __finishLine = 620
+    
+    
+    def __init__(self):
+        self.__screen = pygame.display.set_mode((640, 480))
+        self.background = pygame.image.load("images/background.png")
+        pygame.display.set_caption("Carrera de bichos")
+        
+     
+    def competir(self):
+        
+        gameOver = False
+        
+        while not gameOver:
+            # comprobacion de los eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameOver = True
+            
+            # refrescar / renderizar la pantalla
+            self.__screen.blit(self.background, (0, 0))
+            pygame.display.flip()
+
+        
+        pygame.quit()
+        sys.exit()   
+            
+if __name__ == "__main__":
+    pygame.init()
+    game = Game()
+    game.competir()
